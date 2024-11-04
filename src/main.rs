2e3d8bde5aed0d8263f1f3dafde8e944c2cdef26
Its-Vaibhav-2005 <pandey.vaibhav311005@gmail.com>
2024-11-04 23:07:30 +0530

    commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate flate2;
+
+use flate2::read::GzDecoder;
+use std::env::args;
+use std::fs::File;
+use std::io::copy;
+use std::io::BufReader;
+use std::time::Instant;
+
+fn main(){
+    if args().len() != 3{
+        eprintln!("Usage: `source` `target`");
+        return;
+    }
+    let mut input = BufReader::new(
+        File::open(args().nth(1).unwrap()).unwrap()
+    );
+    let mut out = File::create(
+        args().nth(2).unwrap()
+    ).unwrap();
+    let mut decoder = GzDecoder::new(
+        input
+    );
+    let start = Instant::now();
+    copy(
+        &mut  decoder,
+        &mut out
+    ).unwrap();
+    println!(
+        "Decompressed length: {:?}",
+        out.metadata().unwrap().len()
+    );
+    println!(
+        "Elapsed: {:?}",
+        start.elapsed()
+    );
+}
